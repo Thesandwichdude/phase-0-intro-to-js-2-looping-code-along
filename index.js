ffdffad06064f6d44ec1d8632572e5e7cd5917b1
Kenneth <lopezkenneth2000@yahoo.com>
2021-07-05 12:37:02 -0500

    Completed Assignment(w/a poison ivy rash)

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+
+function writeCards(name){
+    let cards = []
+    for (let i = 0; i < name.length; i++){
+        cards.push(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+    }
+    return cards;
+}
+
+function countDown(numbers) {
+    let countDown = numbers
+    while (countDown >= 0) {
+        console.log(countDown);
+        countDown--
+    }
+}
